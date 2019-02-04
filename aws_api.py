84eb986f208f61ae4a967e3ba3207232c133f585
Daniel Nuriyev <dnuriyev@insightsquared.com>
2019-02-04 08:47:36 -0500

    fix

--- aws_api.py
+++ aws_api.py
@@ -3,8 +3,8 @@ from aws_requests_auth.aws_auth import AWSRequestsAuth
 import json
 import requests
 
-auth = AWSRequestsAuth(aws_access_key="AKIAIHC4P2NEFQUO4WPA",
-                       aws_secret_access_key="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",
+auth = AWSRequestsAuth(aws_access_key="...",
+                       aws_secret_access_key="...",
                        aws_host="68298blcol.execute-api.us-east-1.amazonaws.com",
                        aws_region='us-east-1',
                        aws_service='execute-api')
